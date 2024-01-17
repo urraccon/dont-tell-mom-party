bd63936c4856dcf89c444cbef177e34706914431
urraccon <133591634+urraccon@users.noreply.github.com>
2024-01-17 12:15:40 +0000

    Deploying to gh-pages from @ urraccon/dont-tell-mom-party@390e274857f9dd5f2fef1ca3a5af9e24cbb91648 🚀

---
+++ index.9bf9403f.js
@@ -0,0 +1,2 @@
+document.querySelector("form.purchase-ticket").addEventListener("submit",(function(e){e.preventDefault(),window.open("https://epl.ro/q/NTB22qgyeYtRpArUP9He6V-ko*Of-Ko=")}));
+//# sourceMappingURL=index.9bf9403f.js.map
---
+++ index.d8839eb7.js
@@ -0,0 +1,2 @@
+document.querySelector("form.purchase-ticket").addEventListener("submit",(function(e){e.preventDefault(),window.open("https://epl.ro/q/NTB22qgyeYtRpArUP9He6V-ko*Of-Ko=")}));
+//# sourceMappingURL=index.d8839eb7.js.map
